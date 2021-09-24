9a53b0e383328a650ca6211eab1cab3cfed6bb66
elijahharry <elijah@outbounded.com>
2021-09-24 05:42:20 -0500

    Fixing transition

--- src/components/ContentBlocks/TextImg/TextImg.js
+++ src/components/ContentBlocks/TextImg/TextImg.js
@@ -101,7 +101,7 @@ const TextImg = ({
   return (
     <section
       id={id}
-      key={`textimg-${id}-${Date.now()}-${Math.random()}`}
+      key={`textimg-${id}}`}
       className={`${classes.section}${
         section_border ? ` ${classes.section_border}` : ""
       }`}
